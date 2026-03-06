9fa376022d3bbaf9b72b8a51c3a68613edeb6184
Erik Hofer <git@erikhofer.de>
2026-03-06 18:25:55 +0100

    Use log4j

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,20 @@
 package org.example
 
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
 import java.io.File
 import java.io.InputStream
 import java.nio.file.Files
 import java.nio.file.Path
 import java.util.zip.ZipInputStream
 
+private val logger: Logger = LogManager.getLogger()
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
     val path = Path.of("build/extract")
-    println("Extracting to ${path.toAbsolutePath()}")
+    logger.info("Extracting to ${path.toAbsolutePath()}")
     path.toFile().mkdirs()
     unzip(File("src/main/resources/example.zip").inputStream(), path)
 }
