bc209bdf05c6f9f9085d22e6a170fa74cf0f6ef2
Erik Hofer <git@erikhofer.de>
2026-03-06 17:03:01 +0100

    Add unsafe unzip

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,37 @@
 package org.example
 
+import java.io.File
+import java.io.InputStream
+import java.nio.file.Files
+import java.nio.file.Path
+import java.util.zip.ZipInputStream
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    val path = Path.of("build/extract")
+    println("Extracting to ${path.toAbsolutePath()}")
+    path.toFile().mkdirs()
+    unzip(File("src/main/resources/example.zip").inputStream(), path)
+}
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+fun unzip(zipStream: InputStream, targetDir: Path) {
+    ZipInputStream(zipStream).use { zis ->
+        while (true) {
+            val entry = zis.nextEntry ?: break
+
+            // Unsicher: Entry-Name wird ungeprüft an targetDir angehängt
+            val file = targetDir.resolve(entry.name)
+
+            if (entry.isDirectory) {
+                Files.createDirectories(file)
+            } else {
+                Files.createDirectories(file.parent)
+                Files.newOutputStream(file).use { out ->
+                    zis.copyTo(out)
+                }
+            }
+            zis.closeEntry()
+        }
     }
 }
