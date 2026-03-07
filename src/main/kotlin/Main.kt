9e0437d08301f7efdedb91fe0b04d97f8ae2ce40
Erik Hofer <git@erikhofer.de>
2026-03-07 21:23:51 +0100

    Add ZAP Scan

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,7 +4,6 @@ import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.html.*
 import io.ktor.server.netty.*
-import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import kotlinx.html.*
 import org.apache.logging.log4j.LogManager
@@ -25,7 +24,6 @@ fun main() {
     path.toFile().mkdirs()
     unzip(File("src/main/resources/example.zip").inputStream(), path)
 
-    // Webserver starten
     startWebServer()
 }
 
